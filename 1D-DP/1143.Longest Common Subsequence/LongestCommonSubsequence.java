a795d16cafc5aff511bafbab4ddc83e1d784eff5
Diaa-Essam <studywithdod@gmail.com>
2025-09-03 11:59:41 +0200

    Add LCS Problem

--- 1D-DP/1143.Longest Common Subsequence/LongestCommonSubsequence.java
+++ 1D-DP/1143.Longest Common Subsequence/LongestCommonSubsequence.java
@@ -1,3 +1,31 @@
+/*
+ *   ========================    Thoughts    ========================
+ * Framework for solving the problem:
+ *    1-Define the objective fucnction
+ *       Let dp[i][j] be the length of the longest common subsequence between the first i characters of text1 and the first j characters of text2.
+ *    2-Identify the base cases
+ *       -F(0, j) = 0 >> When the first string is empty
+ *       -F(j, 0) = 0 >> When the second string is empty
+ *   3-Recurrence Relation
+ *       if characters match
+ *          F(i, j) = 1 + F(i - 1, j - 1);
+ *       else
+ *          F(i, j) = Math.max(F(i - 1, j), F(i, j - 1));
+ *   4- Order of computation
+ *       -Buttom-Up
+ *   5- Location of the answer
+ *       F(n, m)
+ *   ========================    complexities    ========================
+ *   Time Complexity: O(n^2)
+ *   Space Complexity: O(n^2)
+ *   ========================    Applications    ========================
+ *   ~ Git/Diff Tools: Find changes between file versions
+ *   ~ DNA Sequence Alignment: Compares genetic sequences in bioinformatics
+ *   ~ Plagiarism Detection
+ *   ~ Auto-Correction
+ * */
+
+
 public class LongestCommonSubsequence
 {
     public int longestCommonSubsequence(String text1, String text2)
