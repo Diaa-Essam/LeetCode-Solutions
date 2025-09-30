60551e94a38860627bb29e66c3c1ab1563295163
Diaa-Essam <studywithdod@gmail.com>
2025-09-30 20:25:16 +0300

    Binary Search

---
+++ Array/704.BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch
+{
+    public int search(int[] nums, int target)
+    {
+        int left = 0, right = nums.length - 1;
+
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if(target > nums[mid])
+            {
+                left = mid + 1;
+            }
+            else if(target < nums[mid]){
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+}
