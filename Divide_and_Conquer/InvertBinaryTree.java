79f09522d00264c1ce393fd40ba9b91cdde7e764
Diaa-Essam <studywithdod@gmail.com>
2025-10-11 09:20:13 +0300

    Added InvertBinaryTree Problem

---
+++ Divide_and_Conquer/InvertBinaryTree.java
@@ -0,0 +1,21 @@
+public class InvertBinaryTree
+{
+    public TreeNode invertTree(TreeNode root) 
+    {
+        if(root == null) return null;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+
+        return root;   
+    }
+}
+/*
+Time: O(n)
+Space: O(h)
+
+Used in Computer Graphics
+ */
