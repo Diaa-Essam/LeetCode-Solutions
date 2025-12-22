1f1434da1623b65a1bfd0669c4fe492d780d20c7
Diaa-Essam <studywithdod@gmail.com>
2025-12-22 06:55:46 +0200

    977.Squares Of A Sorted Array

---
+++ Two-Pointers/SortedSquares.java
@@ -0,0 +1,12 @@
+public class SortedSquares
+{
+    public int[] sortedSquares(int[] nums) 
+    {
+        for(int i = 0; i < nums.length; i++)
+        {
+            nums[i] *= nums[i]; 
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
