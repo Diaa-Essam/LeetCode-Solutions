8a9abbafa1949ae19008efa42f2a151fcf121175
Diaa-Essam <diaaessam042@gmail.com>
2026-01-28 12:00:34 +0200

    739.Daily Temperatures

---
+++ Stack/739.Daily Temperatures/Temperatures.java
@@ -0,0 +1,18 @@
+public class Temperatures
+{
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                result[prevDay] = i - prevDay;
+            }
+            stack.push(i);
+        }
+        
+        return result;
+    }
+}
