609f143216392beb8b82010cd358b0bdcdf9ac80
Diaa-Essam <diaaessam042@gmail.com>
2026-05-11 19:12:49 +0300

    add: 2553.Separate the Digits in an Array

---
+++ Dart/problem.dart
@@ -0,0 +1,17 @@
+//2553. Separate the Digits in an Array
+
+List<int> separateDigits(List<int> nums) {
+  List<int> result = [];
+  for (int i = 0; i < nums.length; i++) {
+    int num = nums[i];
+    List<int> temp = [];
+    while (num != 0) {
+      temp.add(num % 10);
+      num = num ~/ 10;
+    }
+    for (int j = temp.length - 1; j >= 0; j--) {
+      result.add(temp[j]);
+    }
+  }
+  return result;
+}
