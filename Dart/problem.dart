ddda28ac4eec28c3216b3ce8254b7c24723798e9
Diaa-Essam <studywithdod@gmail.com>
2026-05-12 12:19:39 +0300

    1512.Number Of Good Pairs

--- Dart/problem.dart
+++ Dart/problem.dart
@@ -1,5 +1,4 @@
 //2553. Separate the Digits in an Array
-// It's gonna reflect.
 List<int> separateDigits(List<int> nums) {
   List<int> result = [];
   for (int i = 0; i < nums.length; i++) {
@@ -11,6 +10,19 @@ List<int> separateDigits(List<int> nums) {
     }
     for (int j = temp.length - 1; j >= 0; j--) {
       result.add(temp[j]);
+    }
+  }
+  return result;
+}
+
+// 1512.Number Of Good Pairs
+int numIdenticalPairs(List<int> nums) {
+  int result = 0;
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        result++;
+      }
     }
   }
   return result;
