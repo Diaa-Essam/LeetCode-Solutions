2598eb46e8d0f47c5e03c4003b165cdc7f1e0478
Diaa-Essam <studywithdod@gmail.com>
2026-05-13 19:38:59 +0300

    add: 3507.Minimum Pair Removal To Start Array I

--- Dart/problem.dart
+++ Dart/problem.dart
@@ -27,3 +27,36 @@ int numIdenticalPairs(List<int> nums) {
   }
   return result;
 }
+
+//3507. Minimum Pair Removal to Sort Array I
+int minimumPairRemoval(List<int> nums) {
+  int operations = 0;
+
+  while (!isNonDecreasing(nums)) {
+    int minSum = 99999999;
+    int minIndex = 0;
+
+    for (int i = 0; i < nums.length - 1; i++) {
+      int sum = nums[i] + nums[i + 1];
+      if (sum < minSum) {
+        minSum = sum;
+        minIndex = i;
+      }
+    }
+
+    nums[minIndex] += nums[minIndex + 1];
+    nums.removeAt(minIndex + 1);
+    operations++;
+  }
+  return operations;
+}
+
+bool isNonDecreasing(List<int> nums) {
+  for (int i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
