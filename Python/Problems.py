f77aa3bd807d0e58a296cffac83a4c1c8a3a97ee
Diaa Essam <studywithdod@gmail.com>
2026-08-25 11:31:13 +0300

    841. Keys and Rooms

--- Python/Problems.py
+++ Python/Problems.py
@@ -201,4 +201,16 @@ class Solution:
                 if edge_count == k * (k - 1) // 2:
                     count += 1
         return count
+
+# 841. Keys and Rooms
+
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        def dfs(room):
+            visited.add(room)
+            for key in rooms[room]:
+                if key not in visited:
+                    dfs(key)
+        dfs(0)
+        return len(visited) == len(rooms)
                 
